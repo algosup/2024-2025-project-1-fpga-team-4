4c9281bd256810958b847d05e1eb28f33722d4e2
ClementineCurel <78617457+Clementine951@users.noreply.github.com>
2024-10-17 14:25:17 +0200

    Update Frogger.sh

--- Script/Frogger.sh
+++ Script/Frogger.sh
@@ -112,18 +112,6 @@ install_apio_drivers_windows() {
     fi
 }
 
-# Function to check if the script is running with administrator privileges
-is_admin() {
-    net session > /dev/null 2>&1
-    if [ $? -eq 0 ]; then
-        echo "Running as Administrator."
-    else
-        echo "This script requires administrator privileges. Attempting to restart with elevated permissions..."
-        powershell -Command "Start-Process '$0' -Verb runAs"
-        exit 1
-    fi
-}
-
 #######################################################################
 # Main Script
 #######################################################################
@@ -217,9 +205,6 @@ elif [[ "$OS_TYPE" == "Darwin" ]]; then
 
 elif [[ "$OS_TYPE" == "MINGW64_NT"* || "$OS_TYPE" == "MSYS_NT"* ]]; then
     echo "You are running on Windows."
-
-    # Ensure script is run as Administrator
-    is_admin
 
     # Step 1: Check if Python 3.9 or higher is installed, ask the user to install manually if not
     echo "Checking if Python 3.9 or higher is installed..."
